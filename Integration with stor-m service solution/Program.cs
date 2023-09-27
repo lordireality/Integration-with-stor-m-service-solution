9934c2448b157754cd4c4da20f650f12069cc462
lordireality <lordireality@gmail.com>
2023-09-27 09:50:12 +0300

    Добавьте файлы проекта.

---
+++ Integration with stor-m service solution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration_with_stor_m_service_solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
