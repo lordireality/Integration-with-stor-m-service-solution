0d10a2d50cb2558a949c78bdf27d0cb15999bad1
lordireality <lordireality@gmail.com>
2023-09-27 16:44:39 +0300

    ready

--- Integration with stor-m service solution/Program.cs
+++ Integration with stor-m service solution/Program.cs
@@ -18,7 +18,7 @@ namespace Integration_with_stor_m_service_solution
             Console.Write(json);
 
 
-            var provider = new StormIntegration.HttpProvider().MakeRequest("http://192.168.161.4:8080/api/v1/setcard", StormIntegration.RequestType.POST, null, json);
+            var provider = new StormIntegration.HttpProvider().MakeRequest("/api/v1/setcard", StormIntegration.RequestType.POST, null, json);
             Console.WriteLine(provider.success);
             Console.ReadKey();
         }
